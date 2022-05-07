ffb516be82d75f845a39d75606f4ae4b8704f63c
BenceBB <bihacsy.bence@gmail.com>
2022-05-07 17:43:48 +0200

    alap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'main', loadChildren: () => import('./pages/main/main.module').then(m => m.MainModule) }, { path: 'breakfast', loadChildren: () => import('./pages/breakfast/breakfast.module').then(m => m.BreakfastModule) }, { path: 'lunch', loadChildren: () => import('./pages/lunch/lunch.module').then(m => m.LunchModule) }, { path: 'dinner', loadChildren: () => import('./pages/dinner/dinner.module').then(m => m.DinnerModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
